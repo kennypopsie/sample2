10f6c866bdc56e65bb6001b1d161bd22b5ede774
kensuke <mynameiskensuke@gmail.com>
2023-06-15 21:47:37 +0000

    課題rails05-3を追加

---
+++ rails05-3.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  # PostController
+   get 'posts/new',to: 'posts#new', as: 'new_post'
+
+
+   get 'topics/new', to: 'topics#new', as: 'new_topics'
+
+
+   get 'topics/edit', to: 'topics#edit', as: 'edit_topics'
+end 
